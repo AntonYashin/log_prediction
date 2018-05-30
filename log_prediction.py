2397f6fa1d9bdf9d8a23e7fca872d32909898290
Yashin <anton.yashin@accenture.com>
2018-05-30 22:59:27 +0300

    Modify file name. Create main function.

--- log_prediction.py
+++ log_prediction.py
@@ -12,40 +12,40 @@ from sklearn.model_selection import cross_val_score
 parameters = ["Vp", "VsElasticOriginal"]
 
 
-def read_log_info(parameter, well=1):
-    file = os.path.abspath(".\well_log_data\\" + parameter + ".Well.E\\" + parameter + ".Well." + str(well) + ".E.csv")
-    return pd.read_csv(file, delimiter=",", header=None)
+# def read_log_info(parameter, well=1):
+#     # file = os.path.abspath("C:\\Users\\anton.yashin\\log_prediction\\well_log_data\\Vp.Well.E\\Vp.Well.E.1.csv")
+#     return pd.read_csv("C:/Users/anton.yashin/log_prediction/well_log_data/Vp.Well.E/Vp.Well.E.1.csv", delimiter=",", header=None)
 
 
-data = read_log_info(parameters[1], well=1)
+data = pd.read_csv('C:\\Users\\anton.yashin\\log_prediction\\well_log_data\\Vp.Well.E\\Vp.Well.E.1.csv', delimiter=",", header=None)
 depth = data[0].values
 log_information = data[1].values
 
 depth_train, depth_test, log_train, log_test = train_test_split(depth, log_information, test_size=0.25, random_state=42)
 
-# print(os.listdir(os.path.abspath(".\well_log_data\Vp.Well.E")))
-# plt.scatter(depth, log_information)
-# plt.scatter(depth, log_information)
+print(os.listdir(os.path.abspath(".\well_log_data\Vp.Well.E")))
+plt.scatter(depth, log_information)
+plt.scatter(depth, log_information)
+
+plt.show()
 #
-# plt.show()
+# degrees = [1, 5, 10]
 #
-degrees = [1, 5, 10]
-
-for i in range(len(degrees)):
-    # ax = plt.subplot(1, len(degrees), i + 1)
-    plt.setp(ax, xticks=(), yticks=())
-
-    polynomial_features = PolynomialFeatures(degree=degrees[i],
-                                             include_bias=False)
-    linear_regression = LinearRegression()
-    pipeline = Pipeline([("polynomial_features", polynomial_features),
-                         ("linear_regression", linear_regression)])
-    pipeline.fit(depth_train[:, np.newaxis], log_train)
-
-    print(pipeline.steps[1][1].coef_)
-    # Evaluate the models using crossvalidation
-    scores = cross_val_score(pipeline, depth_train[:, np.newaxis], log_train,
-                             scoring="neg_mean_squared_error", cv=10)
+# for i in range(len(degrees)):
+#     # ax = plt.subplot(1, len(degrees), i + 1)
+#     plt.setp(ax, xticks=(), yticks=())
+#
+#     polynomial_features = PolynomialFeatures(degree=degrees[i],
+#                                              include_bias=False)
+#     linear_regression = LinearRegression()
+#     pipeline = Pipeline([("polynomial_features", polynomial_features),
+#                          ("linear_regression", linear_regression)])
+#     pipeline.fit(depth_train[:, np.newaxis], log_train)
+#
+#     print(pipeline.steps[1][1].coef_)
+#     # Evaluate the models using crossvalidation
+#     scores = cross_val_score(pipeline, depth_train[:, np.newaxis], log_train,
+#                              scoring="neg_mean_squared_error", cv=10)
 
     # plt.scatter(depth_test, pipeline.predict(depth_test[:, np.newaxis]), label="Model")
     # plt.plot(depth_test, log_test, label="True function")
